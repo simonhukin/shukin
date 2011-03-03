d9655bc354f98e01e2d343a1a3e5b6e2a6ee1411
simonhukin <simonhukin@gmail.com>
2011-03-03 12:36:50 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shukin::Application.config.secret_token = '435ffe5cad5cef6bdb2ae93ba1f7ae32ddd0352d71203216f31115f1993ef3eab072d5b1e84bb6e2cc7e7f3a9591c490d873b3a44120c753e8d64b6e745eb400'
